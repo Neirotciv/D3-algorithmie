7d368f1b627fe55e78f1df82ef815482e65fc166
VicLam <tanacetifolia@gmail.com>
2022-11-22 11:19:06 +0100

    exercice 3

--- exercice3_lettre.php
+++ exercice3_lettre.php
@@ -1,13 +1,33 @@
 <?php
 
 // Exercice 3 : Recherche lettre
+$tableauMot = [];
 
 // Demander la saisie d’un mot
+echo "Saisir un mot : ";
+$mot = readline();
 
-// Stocker le mot dans un tableau, lettre par
-// lettre
+// Stocker le texte dans un tableau, lettre par lettre
+for ($i = 0; $i < strlen($mot); $i++) {
+    $tableauMot[$i] = $mot[$i];
+}
 
 // Demander la saisie d’une lettre
+echo "Saisir une lettre : ";
+$lettre = readline();
 
-// Retourner le nombre de fois que cette lettre
-// apparaît dans le mot
+
+function rechercherLettre(array $mot, string $lettre): int {
+    $count = 0;
+    
+    // Chercher le nombre d'occurence de la lettre
+    for ($i = 0; $i < count($mot); $i++) {
+        if ($mot[$i] == $lettre) {
+            $count++;
+        }
+    }
+
+    return $count;
+}
+
+echo 'La lettre ' . $lettre . ' apparait ' . rechercherLettre($tableauMot, $lettre) . ' fois';
