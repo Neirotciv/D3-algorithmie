a9c7cd1e1115cc97b168911afd914557c4b4451e
Neirotciv <tanacetifolia@gmail.com>
2022-11-28 23:44:23 +0100

    refacto exercise 9

--- exercice9_dice.php
+++ exercice9_dice.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Exercise 9 : Jeu de dés Yam’s
+ */
+
 function checkEquality(array $dices): bool {
     $firstDice = $dices[0];
     for ($i = 0; $i < count($dices); $i++) {
@@ -10,13 +14,14 @@ function checkEquality(array $dices): bool {
     return true;
 }
 
-function rollTheDice(): void {
-    echo "Lancer les dés\n";
+function rollTheDice(bool $cheat = false): void {
+    echo "Roll the dices\n";
     $totalDices = 5;
     $dices = [];
 
     for ($i = 0; $i < $totalDices; $i++) {
-        $dice = rand(1, 2);
+        $dice = rand(1, 6);
+        if ($cheat == true) { $dice = 6; }
         echo $dice . ' ';
         $dices[] = $dice;
     }
@@ -26,8 +31,16 @@ function rollTheDice(): void {
     }
 }
 
+$cheating = false;
+
 do {
-    rollTheDice();
+    rollTheDice($cheating);
     echo "\nRejouer ? (y/n)\n";
+
     $play = readline();
+
+    if ($play == 'c') { 
+        $cheating = true; 
+        $play = readline(); 
+    }
 } while($play != 'n');
