80c3e55e2b7785374c2ce099d108ed9b94d16384
Neirotciv <tanacetifolia@gmail.com>
2022-12-04 15:21:50 +0100

    exercise 5

--- exercice5_batonnets.php
+++ exercice5_batonnets.php
@@ -4,8 +4,22 @@
  * Jeu des bâtonnets
  */
 
-// Player takes sticks between 1 and 3
-
-// IA takes sticks 
-
 $sticks = 20;
+
+// The player must start first
+do {
+    echo "\nBatons restants " . $sticks . "\n";
+    
+    // Player takes sticks between 1 and 3
+    echo "Joueur ";
+    $playerPickedUp = readline();
+    $sticks -= $playerPickedUp;
+    
+    // For the computer to win, the sum of the sticks picked 
+    // up in a round must be equal to 4
+    $botPickedUp = 4 - $playerPickedUp;
+    echo "Ordinateur " . $botPickedUp . "\n";
+    $sticks -= $botPickedUp;
+
+    echo "L'ordinateur a gagné";
+} while($sticks != 0);
